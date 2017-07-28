436a0a36389622732e70231d5f862333abdbffb5
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-28 14:15:58 -0500

    Add color picker in create note form to apply background color

--- src/app/ui/note-creator.ts
+++ src/app/ui/note-creator.ts
@@ -21,7 +21,7 @@ import {
     }
   `],
   template: `
-    <div class="note-creator shadow-2">
+    <div class="note-creator shadow-2" [style.backgroundColor]="newNote.color">
       <form class="row" (ngSubmit)="onCreateNote()">
         <input
           type="text"
@@ -41,6 +41,12 @@ import {
           class="col-xs-10"
         >
         <div class="actions col-xs-12 row between-xs" *ngIf="fullForm">
+            <div class="col-xs-3">
+                <color-picker 
+                    [colors]="colors"
+                    (selected)="onColorSelect($event)"
+                ></color-picker>
+            </div>
           <button
             type="submit"
             class="btn-light"
@@ -54,17 +60,20 @@ import {
 })
 export class NoteCreator {
   @Output() createNote = new EventEmitter();
+  colors: string[] = ['white', 'black', 'green', 'red', 'blue', 'yellow'];
+
   newNote = {
     title: '',
-    value: ''
+    value: '',
+    color: 'white'
   };
   fullForm: boolean = false;
 
   onCreateNote() {
-    const { title, value } = this.newNote;
+    const { title, value, color } = this.newNote;
 
     if (title && value) {
-      this.createNote.next({ title, value });
+      this.createNote.next({ title, value, color });
     }
 
     this.reset();
@@ -74,11 +83,16 @@ export class NoteCreator {
   reset() {
     this.newNote = {
       title: '',
-      value: ''
+      value: '',
+      color: 'white'
     };
   }
 
   toggle(value: boolean) {
     this.fullForm = value;
+  }
+
+  onColorSelect(color: string) {
+    this.newNote.color = color;
   }
 }
