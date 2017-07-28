fe484c407c647457dcf46c43168da745f6609671
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-28 11:34:02 -0500

    Add app-bar to application with styles

--- src/app/containers/main.ts
+++ src/app/containers/main.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'main-container',
   template: `
-    <div>
-      <main class="main">
-        content will go here
-      </main>
+    <div class="main-container">
+        <app-bar></app-bar>
+        <main class="main">
+            content will go here
+        </main>
     </div>
   `
 })
