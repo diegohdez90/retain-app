8049ae29288ac9f12b90533f7a88ef650330bf91
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-28 11:19:37 -0500

    Add export components at index.ts

---
+++ src/app/containers/main.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'main-container',
+    template : `
+        <div>
+            <main class="main">
+                Content here
+            </main>
+        </div>
+    `
+})
+
+export class Main {};
+
